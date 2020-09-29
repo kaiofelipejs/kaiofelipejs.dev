64c1c7a5bae58299e204fd14338f4673354f007c
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2020-09-29 13:50:03 -0300

    Update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,21 +1,11 @@
 import React from "react"
-import { Link } from "gatsby"
+import Layout from "components/Layout"
+import SEO from "components/seo"
 
 const AboutPage = () => (
-  <>
+  <Layout>
+    <SEO title="About" />
     <h1>About Page</h1>
-    <ul>
-      <li>
-        <Link to="/about" activeStyle={{ color: "red " }}>
-          About
-        </Link>
-      </li>
-      <li>
-        <Link to="/" activeStyle={{ color: "red " }}>
-          Home
-        </Link>
-      </li>
-    </ul>
-  </>
+  </Layout>
 )
 export default AboutPage
