c72a177d9ad26fb1c79e5d9a25135b4e8e385f17
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2020-09-30 19:12:46 -0300

    Add alt text in MenuBar icons

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -17,14 +17,17 @@ const MenuBar = ({ setReadingMode, readingMode }) => {
       <S.MenuBarGroup>
         <S.MenuBarLink to="/" title="Voltar para home">
           <S.MenuBarItem>
-            <Icon name="shdocvw_256" />
+            <Icon
+              name="shdocvw_256"
+              alt="Icone com uma folha ao fundo e uma casa pequena no canto inferior direito."
+            />
             <span>Home</span>
           </S.MenuBarItem>
         </S.MenuBarLink>
 
         <S.MenuBarLink to="/search/" title="Pesquisar">
           <S.MenuBarItem>
-            <Icon name="sccview_icon" />
+            <Icon name="sccview_icon" alt="Icone de uma lupa." />
             <span>Buscar</span>
           </S.MenuBarItem>
         </S.MenuBarLink>
@@ -35,13 +38,19 @@ const MenuBar = ({ setReadingMode, readingMode }) => {
           onClick={toggleReadingMode}
           title={setContentInToggleReadingMode}
         >
-          <Icon name="winhlp32_4001" />
+          <Icon
+            name="winhlp32_4001"
+            alt="Icone de um livro fechado com uma interrogação na capa."
+          />
 
           <span>{setContentInToggleReadingMode}</span>
         </S.MenuBarItem>
 
         <S.MenuBarItem title="Ir para o topo">
-          <Icon name="progman_46" />
+          <Icon
+            name="progman_46"
+            alt="Icone de uma seta apontando para cima."
+          />
           <span>Ir para o topo</span>
         </S.MenuBarItem>
       </S.MenuBarGroup>
