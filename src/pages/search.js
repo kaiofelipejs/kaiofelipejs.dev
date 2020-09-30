19a6fefd321892b611e2715fb797d20d82b50376
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2020-09-30 16:58:29 -0300

    Create initial structure to search page

---
+++ src/pages/search.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "components/Layout"
+import SEO from "components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Search" />
+    <h1>Search</h1>
+  </Layout>
+)
+
+export default IndexPage
