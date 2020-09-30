cda7e8dec54c88a90dbcb14aba0cfdc0d8749a14
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2020-09-30 16:58:39 -0300

    Add lorem content in homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,128 @@ import SEO from "components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="Home | Kaio Felipe Silva" />
     <h1>Home</h1>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi ullam
+      atque, dicta, molestiae voluptas maxime quod tempore exercitationem quasi
+      minima saepe provident doloribus ad aut rem omnis possimus perspiciatis
+      ipsa. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum
+      aperiam dolore, minus ex cum consequatur illum tempora culpa error
+      voluptatibus sapiente id consectetur accusamus nostrum ratione, odit quis
+      vero illo! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Voluptates dicta omnis fuga dolorum! Tempora libero debitis, mollitia
+      eligendi magnam, esse qui voluptatum cum alias dolor suscipit itaque
+      corporis officia consequatur?
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi ullam
+      atque, dicta, molestiae voluptas maxime quod tempore exercitationem quasi
+      minima saepe provident doloribus ad aut rem omnis possimus perspiciatis
+      ipsa. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum
+      aperiam dolore, minus ex cum consequatur illum tempora culpa error
+      voluptatibus sapiente id consectetur accusamus nostrum ratione, odit quis
+      vero illo! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Voluptates dicta omnis fuga dolorum! Tempora libero debitis, mollitia
+      eligendi magnam, esse qui voluptatum cum alias dolor suscipit itaque
+      corporis officia consequatur?
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi ullam
+      atque, dicta, molestiae voluptas maxime quod tempore exercitationem quasi
+      minima saepe provident doloribus ad aut rem omnis possimus perspiciatis
+      ipsa. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum
+      aperiam dolore, minus ex cum consequatur illum tempora culpa error
+      voluptatibus sapiente id consectetur accusamus nostrum ratione, odit quis
+      vero illo! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Voluptates dicta omnis fuga dolorum! Tempora libero debitis, mollitia
+      eligendi magnam, esse qui voluptatum cum alias dolor suscipit itaque
+      corporis officia consequatur?
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi ullam
+      atque, dicta, molestiae voluptas maxime quod tempore exercitationem quasi
+      minima saepe provident doloribus ad aut rem omnis possimus perspiciatis
+      ipsa. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum
+      aperiam dolore, minus ex cum consequatur illum tempora culpa error
+      voluptatibus sapiente id consectetur accusamus nostrum ratione, odit quis
+      vero illo! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Voluptates dicta omnis fuga dolorum! Tempora libero debitis, mollitia
+      eligendi magnam, esse qui voluptatum cum alias dolor suscipit itaque
+      corporis officia consequatur?
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi ullam
+      atque, dicta, molestiae voluptas maxime quod tempore exercitationem quasi
+      minima saepe provident doloribus ad aut rem omnis possimus perspiciatis
+      ipsa. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum
+      aperiam dolore, minus ex cum consequatur illum tempora culpa error
+      voluptatibus sapiente id consectetur accusamus nostrum ratione, odit quis
+      vero illo! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Voluptates dicta omnis fuga dolorum! Tempora libero debitis, mollitia
+      eligendi magnam, esse qui voluptatum cum alias dolor suscipit itaque
+      corporis officia consequatur?
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi ullam
+      atque, dicta, molestiae voluptas maxime quod tempore exercitationem quasi
+      minima saepe provident doloribus ad aut rem omnis possimus perspiciatis
+      ipsa. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum
+      aperiam dolore, minus ex cum consequatur illum tempora culpa error
+      voluptatibus sapiente id consectetur accusamus nostrum ratione, odit quis
+      vero illo! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Voluptates dicta omnis fuga dolorum! Tempora libero debitis, mollitia
+      eligendi magnam, esse qui voluptatum cum alias dolor suscipit itaque
+      corporis officia consequatur?
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi ullam
+      atque, dicta, molestiae voluptas maxime quod tempore exercitationem quasi
+      minima saepe provident doloribus ad aut rem omnis possimus perspiciatis
+      ipsa. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum
+      aperiam dolore, minus ex cum consequatur illum tempora culpa error
+      voluptatibus sapiente id consectetur accusamus nostrum ratione, odit quis
+      vero illo! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Voluptates dicta omnis fuga dolorum! Tempora libero debitis, mollitia
+      eligendi magnam, esse qui voluptatum cum alias dolor suscipit itaque
+      corporis officia consequatur?
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi ullam
+      atque, dicta, molestiae voluptas maxime quod tempore exercitationem quasi
+      minima saepe provident doloribus ad aut rem omnis possimus perspiciatis
+      ipsa. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum
+      aperiam dolore, minus ex cum consequatur illum tempora culpa error
+      voluptatibus sapiente id consectetur accusamus nostrum ratione, odit quis
+      vero illo! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Voluptates dicta omnis fuga dolorum! Tempora libero debitis, mollitia
+      eligendi magnam, esse qui voluptatum cum alias dolor suscipit itaque
+      corporis officia consequatur?
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi ullam
+      atque, dicta, molestiae voluptas maxime quod tempore exercitationem quasi
+      minima saepe provident doloribus ad aut rem omnis possimus perspiciatis
+      ipsa. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum
+      aperiam dolore, minus ex cum consequatur illum tempora culpa error
+      voluptatibus sapiente id consectetur accusamus nostrum ratione, odit quis
+      vero illo! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Voluptates dicta omnis fuga dolorum! Tempora libero debitis, mollitia
+      eligendi magnam, esse qui voluptatum cum alias dolor suscipit itaque
+      corporis officia consequatur?
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi ullam
+      atque, dicta, molestiae voluptas maxime quod tempore exercitationem quasi
+      minima saepe provident doloribus ad aut rem omnis possimus perspiciatis
+      ipsa. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum
+      aperiam dolore, minus ex cum consequatur illum tempora culpa error
+      voluptatibus sapiente id consectetur accusamus nostrum ratione, odit quis
+      vero illo! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Voluptates dicta omnis fuga dolorum! Tempora libero debitis, mollitia
+      eligendi magnam, esse qui voluptatum cum alias dolor suscipit itaque
+      corporis officia consequatur?
+    </p>
   </Layout>
 )
 
