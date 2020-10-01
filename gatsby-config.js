31fd83e0b8129536589629b876d230d43300798a
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2020-10-01 12:33:19 -0300

    Config gatsby-transformer-remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,17 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/posts`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: { plugins: [] },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
