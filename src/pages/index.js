a2a48ef990481e7fcf6445bc9cc8dbe43bf9b61c
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2020-10-01 12:33:38 -0300

    Get posts and create node field

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,59 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "components/Layout"
 import SEO from "components/seo"
 import PostItem from "components/PostItem"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home | Kaio Felipe Silva" />
-    <PostItem
-      slug="/about/"
-      background=""
-      category="Misc"
-      date="01 de Janeiro de 2020"
-      timeToRead="5"
-      title="Titulo muito brabo! É, ç ẽ ê"
-      description="Esse post é top, jamais visto"
-    />
-  </Layout>
-)
+const IndexPage = () => {
+  const { allMarkdownRemark } = useStaticQuery(graphql`
+    query PostList {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              background
+              category
+              date(formatString: "DD [de] MMMM [de] YYYY", locale: "pt-br")
+              description
+              title
+            }
+            timeToRead
+          }
+        }
+      }
+    }
+  `)
+
+  const postList = allMarkdownRemark.edges
+
+  return (
+    <Layout>
+      <SEO title="Home | Kaio Felipe Silva" />
+      {postList.map(
+        ({
+          node: {
+            frontmatter: { background, category, date, title, description },
+            timeToRead,
+            fields: { slug },
+          },
+        }) => (
+          <PostItem
+            slug={slug}
+            background={background}
+            category={category}
+            date={date}
+            timeToRead={timeToRead}
+            title={title}
+            description={description}
+          />
+        )
+      )}
+    </Layout>
+  )
+}
 
 export default IndexPage
