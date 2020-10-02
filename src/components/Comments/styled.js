c793c5c7efa68a6726b4e2bdb21a279f3176f137
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2020-10-01 23:27:27 -0300

    Add comments

---
+++ src/components/Comments/styled.js
@@ -0,0 +1,24 @@
+import styled from "styled-components"
+
+export const CommentsWrapper = styled.section`
+  margin: auto;
+  max-width: 70rem;
+  padding: 3rem 6.4rem 3rem;
+
+  /* #dsq-app5475 {
+    display: none;
+  } */
+
+  #disqus_thread {
+    a {
+      color: red !important;
+    }
+  }
+`
+
+export const CommentsTitle = styled.h2`
+  color: #000;
+  font-size: 2.1rem;
+  font-weight: 700;
+  padding-bottom: 2rem;
+`
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,6 +4,7 @@ import { graphql } from "gatsby"
 import Layout from "components/Layout"
 import SEO from "components/seo"
 import RecommendedPosts from "components/RecommendedPosts"
+import Comments from "components/Comments"
 
 import * as S from "components/Post/styled"
 
@@ -26,6 +27,7 @@ const BlogPost = ({ data, pageContext }) => {
         <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
       </S.MainContent>
       <RecommendedPosts next={next} previous={previous} />
+      <Comments url={post.fields.slug} title={post.frontmatter.title} />
     </Layout>
   )
 }
@@ -33,6 +35,9 @@ const BlogPost = ({ data, pageContext }) => {
 export const query = graphql`
   query Post($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
+      fields {
+        slug
+      }
       frontmatter {
         title
         description
