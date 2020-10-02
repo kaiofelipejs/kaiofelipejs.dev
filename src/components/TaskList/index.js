f60fcac984daacacc7bed92eaf6fe5fa83980873
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2020-10-02 10:00:53 -0300

    Update TaskList

--- src/components/TaskList/index.js
+++ src/components/TaskList/index.js
@@ -5,13 +5,38 @@ import * as S from "./styled"
 
 const TaskList = () => (
   <List>
-    <S.ListLink href="https://github.com/React95/React95">
-      <S.ListItem icon="plugin">React95 Library</S.ListItem>
-    </S.ListLink>
+    <List.Item icon="folder_file">
+      <List>
+        <S.ListLink to="/">
+          <List.Item icon="shdocvw_256">Home</List.Item>
+        </S.ListLink>
+        <S.ListLink to="/about/">
+          <List.Item icon="ulclient_1002">Sobre mim</List.Item>
+        </S.ListLink>
+        <S.ListLink to="/search/">
+          <List.Item icon="sccview_icon">Busca</List.Item>
+        </S.ListLink>
+      </List>
+      Páginas
+    </List.Item>
+    <List.Item icon="bookmark">
+      <List>
+        <List.Item>Ativar</List.Item>
+      </List>
+      Modo leitura
+    </List.Item>
     <List.Divider />
-    <S.ListLink href="https://github.com/kaiofelipejs/personal-site">
-      <S.ListItem icon="computer_3">Source Code</S.ListItem>
-    </S.ListLink>
+    <List.Item icon="settings">
+      <List>
+        <S.ListExternalLink href="https://github.com/kaiofelipejs/kaiofelipejs.dev">
+          <List.Item icon="chatshow_3000">Repositório</List.Item>
+        </S.ListExternalLink>
+        <S.ListExternalLink href="https://github.com/React95/React95">
+          <List.Item icon="brush">React95 lib</List.Item>
+        </S.ListExternalLink>
+      </List>
+      Código fonte
+    </List.Item>
   </List>
 )
 
