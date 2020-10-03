f8087e1d7510ed0c9fba810df8a3e9d1df59d3cd
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2020-10-03 01:16:25 -0300

    Add env in algolia-queries

--- src/utils/algolia_queries.js
+++ src/utils/algolia_queries.js
@@ -35,7 +35,7 @@ const queries = [
   {
     query: postsQuery,
     transformer: ({ data }) => flatten(data.posts.edges),
-    indexName: "Posts",
+    indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
     settings: {
       attributesToSnippet: ["excerpt:20"],
     },
