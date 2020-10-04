2cbc6a558b9f91bcacec331143d56290c7025408
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2020-10-03 23:52:09 -0300

    Style 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,22 @@
 import React from "react"
+import { Link } from "gatsby"
+import { Button } from "@react95/core"
 
 import Layout from "components/Layout"
 import SEO from "components/seo"
 
+import { MainContent } from "components/Post/styled"
+
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>Ops! Nada aqui...</p>
+    <MainContent>
+      <h1>Ops! Nada por aqui...</h1>
+
+      <Link to="/">
+        <Button css={{ fontSize: "1.5rem" }}>Voltar para a home</Button>
+      </Link>
+    </MainContent>
   </Layout>
 )
 
