80e4897e703465320f5e1b489d0b2fd7ca843ef5
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2021-01-24 00:52:16 -0300

    Create SW Updater

---
+++ src/components/SwUpdater/styled.js
@@ -0,0 +1,47 @@
+import styled from "styled-components"
+import { Button } from "@react95/core"
+
+export const NotificationWrapper = styled.div`
+  align-items: center;
+  animation: moveUp 0.5s ease-in-out both;
+  background: var(--mainColor);
+  box-shadow: 0px 0px 25px 2px rgb(195 199 203);
+  color: var(--texts);
+  cursor: pointer;
+  display: flex;
+  justify-content: space-evenly;
+  padding: 20px 10px;
+  position: fixed;
+  right: calc(50vw - 320px / 2);
+  width: 320px;
+  z-index: 10;
+
+  &:hover {
+    transition: 0.1s;
+    filter: brightness(0.9);
+  }
+
+  @keyframes moveUp {
+    0% {
+      bottom: -100vh;
+    }
+    100% {
+      bottom: 60px;
+    }
+  }
+`
+
+export const IconWrapper = styled(Button)`
+  color: var(--texts);
+  height: 30px;
+  width: 30px;
+  padding: 0;
+
+  &:active {
+    padding: 0;
+  }
+
+  & * {
+    cursor: pointer;
+  }
+`
