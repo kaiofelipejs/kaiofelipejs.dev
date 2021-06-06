4a69ad88c68a6c516e08be8e28eeac2d6074e20f
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2021-06-06 15:25:03 -0300

    chore: update avatar image

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -1,29 +1,16 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 
 import * as S from "./styled"
 
-const Avatar = () => {
-  const { avatarImage } = useStaticQuery(
-    graphql`
-      query {
-        avatarImage: file(relativePath: { eq: "profile-photo.jpg" }) {
-          childImageSharp {
-            fluid(maxWidth: 60) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    `
-  )
-
-  return (
-    <S.AvatarWrapper
-      fluid={avatarImage.childImageSharp.fluid}
+const Avatar = () => (
+  <S.AvatarWrapper>
+    <StaticImage
+      src="../../../static/assets/img/profile-photo.jpg"
       alt="Uma foto minha sorrindo de frente para a camera"
+      placeholder="blurred"
     />
-  )
-}
+  </S.AvatarWrapper>
+)
 
 export default Avatar
